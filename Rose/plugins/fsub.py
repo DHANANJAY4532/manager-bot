18098211414ba7ed336549840b7aed0e8ef3ff79
its_me_jupiter <102791126+DHANANJAY4532@users.noreply.github.com>
2022-06-23 19:42:21 +0530

    Update fsub.py

--- Rose/plugins/fsub.py
+++ Rose/plugins/fsub.py
@@ -26,7 +26,7 @@ async def ForceSub(bot: Client, event: Message):
      reply_markup=InlineKeyboardMarkup(
              [
                  [
-                    InlineKeyboardButton("🚀 Join Now  ", url=f"{link}"),
+                    InlineKeyboardButton("Join Now  ", url=f"{link}"),
                  ],
              ]
             ),
@@ -43,5 +43,5 @@ async def ForceSub(bot: Client, event: Message):
         fix_ = await ForceSub(bot, event)
         return fix_
     except Exception as err:
-        print(f"Something Went Wrong! Unable to do Force Subscribe.\nError: {err}\n\nContact Support Group: https://t.me/supunma")
+        print(f"Something Went Wrong!")
         return 200 
